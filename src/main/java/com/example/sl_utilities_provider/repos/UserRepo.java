c2e26f29629e6ef24ac61555dcd30ca2656ced44
Yohan <it20256050@my.sliit.lk>
2022-10-12 14:44:10 +0530

    userRepo interface updated

--- src/main/java/com/example/sl_utilities_provider/repos/UserRepo.java
+++ src/main/java/com/example/sl_utilities_provider/repos/UserRepo.java
@@ -1,10 +1,15 @@
 package com.example.sl_utilities_provider.repos;
 
+import com.example.sl_utilities_provider.entities.Service;
 import com.example.sl_utilities_provider.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
+
+
 
 }
