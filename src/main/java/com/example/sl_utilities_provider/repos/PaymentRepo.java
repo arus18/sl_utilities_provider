fca36d4744d9d3b7aa27bc14795ffd4c24d0571a
DilshanLakindu <dilshanlakindu5@gmail.com>
2022-10-20 13:23:34 +0530

    Payment repo

--- src/main/java/com/example/sl_utilities_provider/repos/PaymentRepo.java
+++ src/main/java/com/example/sl_utilities_provider/repos/PaymentRepo.java
@@ -1,10 +1,17 @@
 package com.example.sl_utilities_provider.repos;
 
 import com.example.sl_utilities_provider.entities.Payment;
+import com.example.sl_utilities_provider.entities.Service;
+import com.example.sl_utilities_provider.entities.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PaymentRepo extends JpaRepository<Payment,Long>{
+
+//    Payment findPaymentByCno(String cnumber);
+//    List<Payment> findCardByHolderName(String cnumber);
 
 }
